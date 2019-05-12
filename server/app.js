1ec2279735f3c0caa4b0d963cd40f65857ffcda4
John Paul Solo <john.solo@jpj.com>
2019-05-12 13:20:40 +0800

    uploading server folder for node js setup for socket: porting to 3001 as sample

---
+++ server/app.js
@@ -0,0 +1,19 @@
+//Created By : John Paul Solo
+//Description : "Backend/Server for socketed Messages and users"
+
+
+const Express = require('express');
+const App = Express();
+
+const server = App.listen(3001 , function() {
+    console.log('working');
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id)
+    socket.on('SEND_MESSAGE', function(data) {
+        io.emit('MESSAGE', data)
+    });
+});
